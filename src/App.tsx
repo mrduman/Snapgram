b3caf73a6e3c7447a6a7b8c2195a8daf468595da
mrduman <omer.duman@gmx.de>
2023-11-27 16:34:42 +0100

    Tailwind has been set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import "./globals.css";
 
 const App = () => {
-  return <div>InstnAp</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
